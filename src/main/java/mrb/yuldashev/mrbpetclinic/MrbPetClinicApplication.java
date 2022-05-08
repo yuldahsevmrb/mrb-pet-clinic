c4f6d0ab3300fdec76aa13051fe9886dd5beb46b
Muzaffar.Yuldashev <yuldashevmrb@gmail.com>
2022-05-08 06:38:32 +0500

    Initial Project Add

---
+++ src/main/java/mrb/yuldashev/mrbpetclinic/MrbPetClinicApplication.java
@@ -0,0 +1,13 @@
+package mrb.yuldashev.mrbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrbPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrbPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mrb/yuldashev/mrbpetclinic/MrbPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mrb.yuldashev.mrbpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrbPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
