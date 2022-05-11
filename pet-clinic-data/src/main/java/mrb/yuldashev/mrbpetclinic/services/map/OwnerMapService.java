92e45862408ec6ef77083990857fcbbf4e213581
Muzaffar.Yuldashev <yuldashevmrb@gmail.com>
2022-05-11 09:20:10 +0500

    Impl Map based services. Closes #6

---
+++ pet-clinic-data/src/main/java/mrb/yuldashev/mrbpetclinic/services/map/OwnerMapService.java
@@ -0,0 +1,37 @@
+package mrb.yuldashev.mrbpetclinic.services.map;
+
+import mrb.yuldashev.mrbpetclinic.model.Owner;
+import mrb.yuldashev.mrbpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+         super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+
+
+
+}
