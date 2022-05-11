3e719a455b9879f73dc105ec748f57db96055d39
Muzaffar.Yuldashev <yuldashevmrb@gmail.com>
2022-05-11 06:51:03 +0500

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/mrb/yuldashev/mrbpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/mrb/yuldashev/mrbpetclinic/services/PetService.java
@@ -3,13 +3,6 @@ package mrb.yuldashev.mrbpetclinic.services;
 
 import mrb.yuldashev.mrbpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/mrb/yuldashev/mrbpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/mrb/yuldashev/mrbpetclinic/services/VetService.java
@@ -2,12 +2,5 @@ package mrb.yuldashev.mrbpetclinic.services;
 
 import mrb.yuldashev.mrbpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
